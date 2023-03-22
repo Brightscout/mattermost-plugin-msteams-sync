8dcb955bf25e3def8f34f809ca6a9561f286e503
Nityanand Rai <nityanand.rai@brightscout.com>
2023-03-22 11:20:40 +0530

    [MI-2907]: Fixed CI errors

--- server/command_test.go
+++ server/command_test.go
@@ -7,9 +7,9 @@ import (
 
 	"bou.ke/monkey"
 	"github.com/mattermost/mattermost-plugin-msteams-sync/server/msteams"
-	mockClient "github.com/mattermost/mattermost-plugin-msteams-sync/server/msteams/mocks"
 	"github.com/mattermost/mattermost-plugin-msteams-sync/server/store"
 	mockStore "github.com/mattermost/mattermost-plugin-msteams-sync/server/store/mocks"
+	mockClient "github.com/mattermost/mattermost-plugin-msteams-sync/server/msteams/mocks"
 	"github.com/mattermost/mattermost-plugin-msteams-sync/server/testutils"
 	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/mattermost/mattermost-server/v6/plugin"
