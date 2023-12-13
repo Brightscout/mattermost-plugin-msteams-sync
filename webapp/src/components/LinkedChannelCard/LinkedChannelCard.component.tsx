95b992003b0120f6f55b4e3ceadde1a80b02d5ce
ayusht2810 <ayush.thakur@brightscout.com>
2023-12-13 21:54:42 +0530

    [MI-3828] Remove extra unit test case and make a common function

--- webapp/src/components/LinkedChannelCard/LinkedChannelCard.component.tsx
+++ webapp/src/components/LinkedChannelCard/LinkedChannelCard.component.tsx
@@ -10,42 +10,41 @@ import {LinkedChannelCardProps} from './LinkedChannelCard.types';
 
 import './LinkedChannelCard.styles.scss';
 
-export const LinkedChannelCard = ({msTeamsChannelName, msTeamsTeamName, mattermostChannelName, mattermostTeamName, mattermostChannelType}: LinkedChannelCardProps) => (
-    <div className='px-16 py-12 border-t-1 d-flex gap-4 msteams-linked-channel'>
-        <div className='msteams-linked-channel__link-icon d-flex align-items-center flex-column justify-center'>
-            <Icon iconName='link'/>
-        </div>
-        <div className='d-flex flex-column gap-6 msteams-linked-channel__body'>
-            <div className='d-flex gap-8 align-items-center'>
-                <Icon iconName={mattermostChannelType === MMConstants.PRIVATE_CHANNEL ? 'lock' : 'globe'}/>
+export const LinkedChannelCard = ({msTeamsChannelName, msTeamsTeamName, mattermostChannelName, mattermostTeamName, mattermostChannelType}: LinkedChannelCardProps) => {
+    const getData = (channelName: string, teamName: string) => {
+        return (
+            <>
                 <Tooltip
                     placement='left'
-                    text={mattermostChannelName}
+                    text={channelName}
                 >
-                    <h5 className='my-0 msteams-linked-channel__entity-label'>{mattermostChannelName}</h5>
+                    <h5 className='my-0 msteams-linked-channel__entity-label'>{channelName}</h5>
                 </Tooltip>
                 <Tooltip
                     placement='left'
-                    text={mattermostTeamName}
+                    text={teamName}
                 >
-                    <h5 className='my-0 opacity-6 msteams-linked-channel__entity-label'>{mattermostTeamName}</h5>
+                    <h5 className='my-0 opacity-6 msteams-linked-channel__entity-label'>{teamName}</h5>
                 </Tooltip>
+            </>
+        );
+    };
+
+    return (
+        <div className='px-16 py-12 border-t-1 d-flex gap-4 msteams-linked-channel'>
+            <div className='msteams-linked-channel__link-icon d-flex align-items-center flex-column justify-center'>
+                <Icon iconName='link'/>
             </div>
-            <div className='d-flex gap-8 align-items-center'>
-                <Icon iconName='msTeams'/>
-                <Tooltip
-                    placement='left'
-                    text={msTeamsChannelName}
-                >
-                    <h5 className='my-0 msteams-linked-channel__entity-label'>{msTeamsChannelName}</h5>
-                </Tooltip>
-                <Tooltip
-                    placement='left'
-                    text={msTeamsTeamName}
-                >
-                    <h5 className='my-0 opacity-6 msteams-linked-channel__entity-label'>{msTeamsTeamName}</h5>
-                </Tooltip>
+            <div className='d-flex flex-column gap-6 msteams-linked-channel__body'>
+                <div className='d-flex gap-8 align-items-center'>
+                    <Icon iconName={mattermostChannelType === MMConstants.PRIVATE_CHANNEL ? 'lock' : 'globe'}/>
+                    {getData(mattermostChannelName, mattermostTeamName)}
+                </div>
+                <div className='d-flex gap-8 align-items-center'>
+                    <Icon iconName='msTeams'/>
+                    {getData(msTeamsChannelName, msTeamsTeamName)}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
